3545fb5f2896ec5c5864cc85bdcb29d48017ddd2
Mike Edovin <edovinmihail@gmail.com>
2022-03-28 13:37:37 +0300

    added control from command line

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -11,9 +11,7 @@ import Users.UsersProvider;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.logging.Logger;
@@ -24,11 +22,78 @@ public class App {
     private static final int PRIORITY_FOR_RECEIVER = 3;
 
     public static void main( String[] args ) throws TelegramApiException {
-        Bot bot = new Bot("EdovinWeatherBot", "5295256851:AAHhA46BhJUzvzIE29rpjE8KlVCsDobF-is");
         UsersProvider usersProvider = new UsersProvider();
         usersProvider.getUsersFromBase();
         ArrayList<User> users = usersProvider.getUsers();
 
+        while (true) {
+            String command = getCommand();
+            switch (command) {
+                case "start":
+                    String token=getBotToken();
+                    Bot bot = new Bot("EdovinWeatherBot", token);
+                    MessageReceiver messageReceiver = new MessageReceiver(bot, usersProvider);
+                    MessageSender messageSender = new MessageSender(bot);
+                    Notify notify = new Notify(bot, usersProvider);
+                    bot.botConnect();
+                    sendStartReport(bot, "5277149986");
+
+                    Thread receiver = new Thread(messageReceiver);
+                    receiver.setDaemon(true);
+                    receiver.setName("MsgReceiver");
+                    receiver.setPriority(PRIORITY_FOR_RECEIVER);
+                    receiver.start();
+
+                    Thread sender = new Thread(messageSender);
+                    sender.setDaemon(true);
+                    sender.setName("MsgSender");
+                    sender.setPriority(PRIORITY_FOR_SENDER);
+                    sender.start();
+
+                    Thread notifyThread = new Thread(notify);
+                    notifyThread.setDaemon(true);
+                    notifyThread.setName("NotifyThread");
+                    notifyThread.start();
+                    break;
+                case "backup":
+                    backup(users);
+                    break;
+                case "showUsers":
+                    showUsers(users);
+                    break;
+                case "exit":
+                    System.exit(0);
+                default:
+                    System.out.println("Wrong input");
+                    break;
+            }
+        }
+
+
+    }
+
+
+    private static void sendStartReport(Bot bot, String BOT_ADMIN) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(BOT_ADMIN);
+        sendMessage.setText("Запустился");
+        bot.sendQueue.add(sendMessage);
+    }
+
+    public static String getCommand(){
+        String command;
+        System.out.print("enter command start, showUsers, backup or exit: ");
+        try{
+            BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+            command=br.readLine();
+            return command;
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public static void showUsers(ArrayList<User>users){
         for (User user : users) {
             System.out.println(user.getUserID());
             if (user.getNotificationTime() != null) {
@@ -46,60 +111,21 @@ public class App {
             }
 
         }
-        System.out.println("local time " + LocalTime.now());
-
-
-        MessageReceiver messageReceiver = new MessageReceiver(bot, usersProvider);
-        MessageSender messageSender = new MessageSender(bot);
-        Notify notify = new Notify(bot, usersProvider);
-        bot.botConnect();
-        sendStartReport(bot, "5277149986");
-
-        Thread receiver = new Thread(messageReceiver);
-        receiver.setDaemon(true);
-        receiver.setName("MsgReceiver");
-        receiver.setPriority(PRIORITY_FOR_RECEIVER);
-        receiver.start();
-
-        Thread sender = new Thread(messageSender);
-        sender.setDaemon(true);
-        sender.setName("MsgSender");
-        sender.setPriority(PRIORITY_FOR_SENDER);
-        sender.start();
-
-        Thread notifyThread = new Thread(notify);
-        notifyThread.setDaemon(true);
-        notifyThread.setName("NotifyThread");
-        notifyThread.start();
-/*
-        while (true) {
-            String command = getCommand();
-            switch (command) {
-                case exit:
-                    System.exit(0);
-
-            }
-        }
-
- */
     }
-
-
-    private static void sendStartReport(Bot bot, String BOT_ADMIN) {
-        SendMessage sendMessage = new SendMessage();
-        sendMessage.setChatId(BOT_ADMIN);
-        sendMessage.setText("Запустился");
-        bot.sendQueue.add(sendMessage);
-    }
-
-    public static String getCommand(){
-        String command;
-        System.out.println("enter command start, save or exit: ");
-        try(BufferedReader br=new BufferedReader(new InputStreamReader(System.in))){
-            command=br.readLine();
-            return command;
+    public static void backup(ArrayList<User>users){
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("usersBackup.txt", false))) {
+            oos.writeObject(users);
+        } catch (IOException e) {
+            log.warning(e.getMessage());
         }
-        catch (IOException e){
+    }
+    public static String getBotToken(){
+        String token;
+        try(BufferedReader br=new BufferedReader(new FileReader("botToken.txt"))){
+            token=br.readLine();
+            System.out.println(token);
+            return token;
+        }catch (IOException e){
             e.printStackTrace();
         }
         return null;
